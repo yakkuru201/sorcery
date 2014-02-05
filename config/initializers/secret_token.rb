3a155fec630651e8b50c8580683707b832e4c8d4
yakkuru201 <tonamimagic@gmail.com>
2014-02-05 22:15:18 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sorcery::Application.config.secret_key_base = 'fd87c1e3c57c19e36c8cf42491b232db66df5b43d315443a3a0d58471f4ae604fedb0b2c138f092a9ed5343d018711ed89937ad6d86adab286fed5ab09bb359d'
