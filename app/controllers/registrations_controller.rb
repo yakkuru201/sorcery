845ae50d6c94cdc376615e19d21cb3bbf256ac43
yakkuru201 <tonamimagic@gmail.com>
2014-02-09 01:49:30 +0900

    define registrations#create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,22 @@ class RegistrationsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new params_user
+
+    if @user.save
+      redirect_to new_registrations_path, notice: "you've signed up."
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:username, :email, :password)
   end
 end
