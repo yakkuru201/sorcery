75f4d33cd889a6916cea9af71e6684e920acce45
yakkuru201 <tonamimagic@gmail.com>
2014-02-09 02:13:58 +0900

    define sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params_user
+
+    if login @user.username, @user.password
+      redirect_to new_sessions_path, notice: "you've signed in."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:username, :password)
   end
 end
