400939740fe5b75b01dcad02f85ba529ec45ca3f
yakkuru201 <tonamimagic@gmail.com>
2014-02-09 01:52:21 +0900

    define routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Sorcery::Application.routes.draw do
 
-  get "sessions/new"
   resource :registrations
+  resource :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
